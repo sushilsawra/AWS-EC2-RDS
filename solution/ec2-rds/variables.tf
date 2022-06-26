6f5284fa0ac0077196cea0515f3f82f6d68a7247
sushilsawra <106230976+sushilsawra@users.noreply.github.com>
2022-06-26 17:10:57 +0530

    Add files via upload

---
+++ solution/ec2-rds/variables.tf
@@ -0,0 +1,20 @@
+
+variable "aws_region" {
+    default = "ap-south-1"
+}
+
+
+variable "ec2_count" {
+  default = "1"
+}
+
+variable "ami_id" {
+    default = "ami-068257025f72f470d"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+// variable "subnet_id" {}
+
